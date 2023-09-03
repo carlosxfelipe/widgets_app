7fb40460b5be0a97135e5704f52a6f69bb03481c
Carlos Felipe Araujo <carlosxfelipe@gmail.com>
2023-09-03 14:00:51 -0300

    provider

--- lib/presentation/screens/counter/counter_screen.dart
+++ lib/presentation/screens/counter/counter_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:widgets_app/presentation/providers/counter_provider.dart';
+import 'package:widgets_app/presentation/providers/theme_provider.dart';
 
 class CounterScreen extends ConsumerWidget {
   static const name = "counter_screen";
@@ -10,10 +11,20 @@ class CounterScreen extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final int clickCounter = ref.watch(counterProvider);
+    final bool isDarkMode = ref.watch(isDarkmodeProvider);
 
     return Scaffold(
       appBar: AppBar(
         title: const Text("Contador Riverpod"),
+        actions: [
+          IconButton(
+              icon: Icon(isDarkMode
+                  ? Icons.dark_mode_outlined
+                  : Icons.light_mode_outlined),
+              onPressed: () {
+                ref.read(isDarkmodeProvider.notifier).update((state) => !state);
+              })
+        ],
       ),
       body: Center(
         child: Text(
@@ -23,7 +34,10 @@ class CounterScreen extends ConsumerWidget {
       ),
       floatingActionButton: FloatingActionButton(
         child: const Icon(Icons.add),
-        onPressed: () {},
+        onPressed: () {
+          // ref.read(counterProvider.notifier).update((state) => state + 1);
+          ref.read(counterProvider.notifier).state++;
+        },
       ),
     );
   }
