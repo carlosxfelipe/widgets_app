2c2f20a2162370b38f6e6df1743f255d6151aafd
Carlos Felipe Araujo <carlosxfelipe@gmail.com>
2023-09-02 22:45:54 -0300

    riverpod

--- lib/config/menu/menu_items.dart
+++ lib/config/menu/menu_items.dart
@@ -15,6 +15,11 @@ class MenuItem {
 
 const appMenuItems = <MenuItem>[
   MenuItem(
+      title: "Contador Riverpod",
+      subTitle: "Introdução a Riverpod",
+      link: "/counter-river",
+      icon: Icons.add),
+  MenuItem(
       title: "Botões",
       subTitle: "Vários botões em Flutter",
       link: "/buttons",
@@ -53,5 +58,5 @@ const appMenuItems = <MenuItem>[
       title: "InfiniteScroll e Pull",
       subTitle: "Listas infinitas e pull to refresh",
       link: "/infinite",
-      icon: Icons.list_alt_rounded)
+      icon: Icons.list_alt_rounded),
 ];
